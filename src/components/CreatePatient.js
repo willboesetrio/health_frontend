4e834aeb63480915a32e72e28ed9eadc2a1004f9
willboesetrio <95431444+willboesetrio@users.noreply.github.com>
2023-04-30 17:48:06 -0400

    Update CreatePatient.js

--- src/components/CreatePatient.js
+++ src/components/CreatePatient.js
@@ -1,10 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from './CreatePatient.module.css';
 
 function CreatePatient() {
-  const usStatesNoId = ['Alabama', 'Alaska', 'American Samoa', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 'District of Columbia', 'Florida', 'Georgia', 'Guam', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Marshall Islands', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota', 'Northern Mariana Islands', 'Ohio', 'Oklahoma', 'Oregon', 'Palau', 'Pennsylvania', 'Puerto Rico', 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virgin Island', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'];
+  const usStatesNoId = ['AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'FL', 'GA', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 'WI', 'WY'];
   const usStates = usStatesNoId.map((s, index) => ({ state: s, id: index }));
   const genderOptions = [{ gender: 'Male', id: 1 }, { gender: 'Female', id: 2 }, { gender: 'Other', id: 3 }];
+  // Input Variables
   const [firstName, setFirstName] = useState('');
   const [lastName, setLastName] = useState('');
   const [ssn, setSsn] = useState('');
@@ -16,9 +17,85 @@ function CreatePatient() {
   const [height, setHeight] = useState('');
   const [weight, setWeight] = useState('');
   const [insurance, setInsurance] = useState('');
-  const [usState, setUsState] = useState('Alabama');
+  const [usState, setUsState] = useState('AL');
   const [gender, setGender] = useState('Male');
   const [formClicked, setFormClicked] = useState(false);
+  // Error Variables
+  const [firstNameError, setFirstNameError] = useState(false);
+  const [lastNameError, setLastNameError] = useState(false);
+  const [ssnError, setSsnError] = useState(false);
+  const [streetError, setStreetError] = useState(false);
+  const [cityError, setCityError] = useState(false);
+  const [emailError, setEmailError] = useState(false);
+  const [postalError, setPostalError] = useState(false);
+  const [ageError, setAgeError] = useState(false);
+  const [heightError, setHeightError] = useState(false);
+  const [weightError, setWeightError] = useState(false);
+  const [insuranceError, setInsuranceError] = useState(false);
+  useEffect(() => {
+    // Regular Expressions
+    const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
+    const alphabeticRegex = /^[a-zA-Z][\\'\\-\\ ]*$/g;
+    // const specificSsnRegex = /^(?!666|000|9\d{2})\d{3}-(?!00)\d{2}-(?!0{4})\d{4}$/;
+    const ssnRegex = /^\d{3}-\d{2}-\d{4}$/;
+    const zipRegex1 = /^\d{5}$/;
+    const zipRegex2 = /^\d{5}-\d{4}$/;
+    if (!firstName.match(alphabeticRegex)) {
+      setFirstNameError(true);
+    } else {
+      setFirstNameError(false);
+    }
+    if (!lastName.match(alphabeticRegex)) {
+      setLastNameError(true);
+    } else {
+      setLastNameError(false);
+    }
+    if (!ssn.match(ssnRegex)) {
+      setSsnError(true);
+    } else {
+      setSsnError(false);
+    }
+    if (!email.match(emailRegex)) {
+      setEmailError(true);
+    } else {
+      setEmailError(false);
+    }
+    if (!street) {
+      setStreetError(true);
+    } else {
+      setStreetError(false);
+    }
+    if (!city) {
+      setCityError(true);
+    } else {
+      setCityError(false);
+    }
+    if (!postal.match(zipRegex1) || !postal.match(zipRegex2)) {
+      setPostalError(true);
+    } else {
+      setPostalError(false);
+    }
+    if (age < 0) {
+      setAgeError(true);
+    } else {
+      setAgeError(false);
+    }
+    if (!height) {
+      setHeightError(true);
+    } else {
+      setHeightError(false);
+    }
+    if (!weight) {
+      setWeightError(true);
+    } else {
+      setWeightError(false);
+    }
+    if (!insurance) {
+      setInsuranceError(true);
+    } else {
+      setInsuranceError(false);
+    }
+  }, [firstName, lastName, ssn, email, street, city, postal, age, height, weight, insurance]);
   const handleCreate = () => {
     setFormClicked(true);
     const payloadObject = {
@@ -42,7 +119,7 @@ function CreatePatient() {
     <div style={{ width: 400, marginLeft: 200 }}>
       <h3>CREATE NEW PATIENT</h3>
       <div className={styles.container}>
-        {formClicked && <p style={{ marginRight: 100 }}>FORM WAS CLICKED</p>}
+        {formClicked && firstNameError && <p style={{ marginRight: 100 }}>error</p>}
         <div className={styles.formrow}>
           <label htmlFor="firstname">
             First Name:
@@ -56,6 +133,7 @@ function CreatePatient() {
             />
           </label>
         </div>
+        {formClicked && lastNameError && <p style={{ marginRight: 100 }}>error</p>}
         <div className={styles.formrow}>
           <label htmlFor="lastname">
             Last Name:
@@ -69,6 +147,7 @@ function CreatePatient() {
             />
           </label>
         </div>
+        {formClicked && ssnError && <p style={{ marginRight: 100 }}>error</p>}
         <div className={styles.formrow}>
           <label htmlFor="ssn">
             SSN:
@@ -82,6 +161,7 @@ function CreatePatient() {
             />
           </label>
         </div>
+        {formClicked && emailError && <p style={{ marginRight: 100 }}>error</p>}
         <div className={styles.formrow}>
           <label htmlFor="email">
             Email:
@@ -95,6 +175,7 @@ function CreatePatient() {
             />
           </label>
         </div>
+        {formClicked && streetError && <p style={{ marginRight: 100 }}>error</p>}
         <div className={styles.formrow}>
           <label htmlFor="street">
             Street Address:
@@ -108,6 +189,7 @@ function CreatePatient() {
             />
           </label>
         </div>
+        {formClicked && cityError && <p style={{ marginRight: 100 }}>error</p>}
         <div className={styles.formrow}>
           <label htmlFor="city">
             City:
@@ -136,6 +218,7 @@ function CreatePatient() {
             </select>
           </label>
         </div>
+        {formClicked && postalError && <p style={{ marginRight: 100 }}>error</p>}
         <div className={styles.formrow}>
           <label htmlFor="postal">
             postal Code:
@@ -149,6 +232,7 @@ function CreatePatient() {
             />
           </label>
         </div>
+        {formClicked && ageError && <p style={{ marginRight: 100 }}>error</p>}
         <div className={styles.formrow}>
           <label htmlFor="age">
             Age:
@@ -162,6 +246,7 @@ function CreatePatient() {
             />
           </label>
         </div>
+        {formClicked && heightError && <p style={{ marginRight: 100 }}>error</p>}
         <div className={styles.formrow}>
           <label htmlFor="height">
             Height(inches):
@@ -175,6 +260,7 @@ function CreatePatient() {
             />
           </label>
         </div>
+        {formClicked && weightError && <p style={{ marginRight: 100 }}>error</p>}
         <div className={styles.formrow}>
           <label htmlFor="weight">
             Weight(lbs):
@@ -188,6 +274,7 @@ function CreatePatient() {
             />
           </label>
         </div>
+        {formClicked && insuranceError && <p style={{ marginRight: 100 }}>error</p>}
         <div className={styles.formrow}>
           <label htmlFor="insurance">
             Insurance:
