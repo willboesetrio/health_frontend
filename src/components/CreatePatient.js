62297c4c815f1786c6ef753cdad6590004fd8a6d
willboesetrio <95431444+willboesetrio@users.noreply.github.com>
2023-04-30 11:42:40 -0400

    Update CreatePatient.js

--- src/components/CreatePatient.js
+++ src/components/CreatePatient.js
@@ -2,23 +2,47 @@ import React, { useState } from 'react';
 import styles from './CreatePatient.module.css';
 
 function CreatePatient() {
-  const usStates = ['Alabama', 'Alaska', 'American Samoa', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 'District of Columbia', 'Florida', 'Georgia', 'Guam', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Marshall Islands', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota', 'Northern Mariana Islands', 'Ohio', 'Oklahoma', 'Oregon', 'Palau', 'Pennsylvania', 'Puerto Rico', 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virgin Island', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'];
-  const genderOptions = ['Male', 'Female', 'Other'];
+  const usStatesNoId = ['Alabama', 'Alaska', 'American Samoa', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 'District of Columbia', 'Florida', 'Georgia', 'Guam', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Marshall Islands', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota', 'Northern Mariana Islands', 'Ohio', 'Oklahoma', 'Oregon', 'Palau', 'Pennsylvania', 'Puerto Rico', 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virgin Island', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'];
+  const usStates = usStatesNoId.map((s, index) => ({ state: s, id: index }));
+  const genderOptions = [{ gender: 'Male', id: 1 }, { gender: 'Female', id: 2 }, { gender: 'Other', id: 3 }];
   const [firstName, setFirstName] = useState('');
   const [lastName, setLastName] = useState('');
   const [ssn, setSsn] = useState('');
   const [email, setEmail] = useState('');
   const [street, setStreet] = useState('');
   const [city, setCity] = useState('');
-  const [zip, setZip] = useState('');
+  const [postal, setPostal] = useState('');
   const [age, setAge] = useState('');
   const [height, setHeight] = useState('');
   const [weight, setWeight] = useState('');
   const [insurance, setInsurance] = useState('');
+  const [usState, setUsState] = useState('Alabama');
+  const [gender, setGender] = useState('Male');
+  const [formClicked, setFormClicked] = useState(false);
+  const handleCreate = () => {
+    setFormClicked(true);
+    const payloadObject = {
+      firstName,
+      lastName,
+      ssn,
+      email,
+      age,
+      height,
+      weight,
+      insurance,
+      gender,
+      street,
+      city,
+      state: usState,
+      postal
+    };
+    console.log(payloadObject);
+  };
   return (
-    <div style={{ width: 400, 'margin-left': 200 }}>
+    <div style={{ width: 400, marginLeft: 200 }}>
       <h3>CREATE NEW PATIENT</h3>
       <div className={styles.container}>
+        {formClicked && <p style={{ marginRight: 100 }}>FORM WAS CLICKED</p>}
         <div className={styles.formrow}>
           <label htmlFor="firstname">
             First Name:
@@ -100,21 +124,28 @@ function CreatePatient() {
         <div className={styles.formrow}>
           <label htmlFor="state">
             State:
-            <select id="state" name="state" className={styles.input} style={{ width: 177 }}>
-              {usStates.map((state) => <option value={state}>{state}</option>)}
+            <select
+              id="state"
+              name="state"
+              className={styles.input}
+              value={usState}
+              onChange={(e) => setUsState(e.target.value)}
+              style={{ width: 177 }}
+            >
+              {usStates.map((s) => <option key={s.id} value={s.state}>{s.state}</option>)}
             </select>
           </label>
         </div>
         <div className={styles.formrow}>
-          <label htmlFor="zip">
-            Zip Code:
+          <label htmlFor="postal">
+            postal Code:
             <input
               type="text"
-              id="zip"
-              name="zip"
+              id="postal"
+              name="postal"
               className={styles.input}
-              value={zip}
-              onChange={(e) => setZip(e.target.value)}
+              value={postal}
+              onChange={(e) => setPostal(e.target.value)}
             />
           </label>
         </div>
@@ -173,13 +204,22 @@ function CreatePatient() {
         <div className={styles.formrow}>
           <label htmlFor="gender">
             Gender:
-            <select id="gender" name="gender" className={styles.input} style={{ width: 177 }}>
-              {genderOptions.map((state) => <option value={state}>{state}</option>)}
+            <select
+              id="gender"
+              name="gender"
+              value={gender}
+              onChange={(e) => setGender(e.target.value)}
+              className={styles.input}
+              style={{ width: 177 }}
+            >
+              {
+              genderOptions.map((g) => <option key={g.id} value={g.gender}>{g.gender}</option>)
+              }
             </select>
           </label>
         </div>
       </div>
-      <button type="button" className={styles.btn}>CREATE</button>
+      <button type="button" className={styles.btn} onClick={() => handleCreate()}>CREATE</button>
     </div>
   );
 }
